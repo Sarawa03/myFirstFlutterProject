d04ce7bf1fcbccc29826045c84e547fd18a774f6
Sarawa03 <saraying03sb@gmail.com>
2023-05-22 18:07:58 +0200

    forgot password view, chap 44 38:50

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -9,6 +9,45 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       : super(const AuthStateUnintialized(isLoading: true)) {
     //Send email verification
 
+    on<AuthEventForgotPassword>(
+      (event, emit) async {
+        emit(const AuthStateForgotPassword(
+          exception: null,
+          hasSentEmail: false,
+          isLoading: false,
+        ));
+
+        final email = event.email;
+        if (email == null) {
+          return; //user just wants to go to forgot-password scren
+        }
+
+//user wants to actually send a forgot-password email
+        emit(const AuthStateForgotPassword(
+          exception: null,
+          hasSentEmail: false,
+          isLoading: true,
+        ));
+
+        bool didSendEmail;
+        Exception? exception;
+        try {
+          await provider.sendPasswordReset(toEmail: email);
+          didSendEmail = true;
+          exception = null;
+        } on Exception catch (e) {
+          didSendEmail = false;
+          exception = e;
+        }
+
+        emit(AuthStateForgotPassword(
+          exception: exception,
+          hasSentEmail: didSendEmail,
+          isLoading: false,
+        ));
+      },
+    );
+
     on<AuthEventSendEmailVerification>(
       (event, emit) async {
         await provider.sendEmailVerification();
